81df7aff0b722dc6a9d0dae9463abf3b21530aff
Armanhakobyan2005 <arman.hakobyan1.g@tumo.org>
2021-06-10 16:37:47 +0400

    asd

---
+++ class.js
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>URL</key>
+	<string>https://gm.tumo.world/file/get?id=60967f74493ac560480e5ad2</string>
+</dict>
+</plist>
